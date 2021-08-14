58a3352d37afa7b868b82c49469dd19555600e41
baixotu <82835424+baixotu@users.noreply.github.com>
2021-08-14 19:20:44 -0300

    Add files via upload
    Brazilian Portuguese:
    Script para manutenção de distribuições Linux baseadas em Debian.
    O script basicamente atualiza o Sistema e deteta logs para aumento de espaço no disco.
    Usei esse script em meu Debian Linux durante anos, não tive problemas.
    Leia o script, analise e use conforme suas preferências, com cuidado. Se conseguir aprimorar: Me passe o código.

---
+++ manutencao.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script para manutenção básica do Debian Linux e derivados.
+# Salvo no diretório: /usr/local/bin
+# chmod +x manutencao.sh
+# Escrito no VI.
+# @baixotu 14/08/2021. 
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+apt update
+apt upgrade
+apt dist-upgrade
+apt full-upgrade
+apt autoremove
+apt autoclean
+apt clean
+cd /var/log
+rm -f *.gz *.old *.0 *.1 
+cd /var/log/apt
+rm -f *.gz
+mandb
+updatedb
+fi
+exit 0
+
